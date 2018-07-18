04f0e3365541b6a230b71df4f20911fab2e4f5b3
braidy <braidy.merkle@trunkclub.com>
2018-07-18 16:27:31 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Oh hey, guise. This is a site I whipped up real quick.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
